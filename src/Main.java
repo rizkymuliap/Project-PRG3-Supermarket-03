fc2c5581c48965cb227b0177f729402f9b62769e
rizkymuliap <perdanarizkymulia162@gmail.com>
2023-06-12 10:43:42 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        Maven dependency for JSON-simple:
+            <dependency>
+                <groupId>com.googlecode.json-simple</groupId>
+                <artifactId>json-simple</artifactId>
+                <version>1.1.1</version>
+            </dependency>
+         */
+
+        try {
+            //Public API:
+            //https://www.metaweather.com/api/location/search/?query=<CITY>
+            //https://www.metaweather.com/api/location/44418/
+
+            URL url = new URL("https://www.metaweather.com/api/location/44418/");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Check if connect is made
+            int responseCode = conn.getResponseCode();
+
+            // 200 OK
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                //Close the scanner
+                scanner.close();
+
+                System.out.println(informationString);
+
+
+                //JSON simple library Setup with Maven is used to convert strings to JSON
+                JSONParser parse = new JSONParser();
+                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
+
+                //Get the first JSON object in the JSON array
+                System.out.println(dataObject.get(0));
+
+                JSONObject countryData = (JSONObject) dataObject.get(0);
+
+                System.out.println(countryData.get("woeid"));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
